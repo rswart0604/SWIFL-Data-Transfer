f3142ba10e9d2abc45be32b06f0c6dcf8328c5ac
rswart0604 <rswart0604@gmail.com>
2024-07-09 15:30:38 -0700

    more variables in arrays. need to put them into Datasets

--- src/WRF_ICARUS/wrf_to_daymet.py
+++ src/WRF_ICARUS/wrf_to_daymet.py
@@ -2,11 +2,6 @@
 the purpose of this script is to take wrf output and turn it into
     daymet format. same variable types, etc. not the same resolution, etc
     this will create multiple nc files
-
-i am leaving out the x and y as they are likely not used and only
-    correlate to the specific projection of the model. use lat and lon
-    instead!!
-
 """
 import xarray as xr
 import wrf
@@ -16,7 +11,7 @@ import numpy as np
 
 WRF_FP = '/scratch/rswart/wrfout_d03_2020-05-16_00_1km_ctrl.nc'
 
-def daylength(dayOfYear, lat):
+def get_day_length(dayOfYear, lat):
     latInRad = np.deg2rad(lat)
     declinationOfEarth = 23.45 * np.sin(np.deg2rad(360.0 * (283.0 + dayOfYear) / 365.0))
     if -np.tan(latInRad) * np.tan(np.deg2rad(declinationOfEarth)) <= -1.0:
@@ -28,20 +23,23 @@ def daylength(dayOfYear, lat):
         return int((2.0 * hourAngle / 15.0) * 60)
 
 
-# precip first. easiest. ish
+# open up our wrf output
 ds = xr.open_dataset(WRF_FP)
 wrfin = Dataset(WRF_FP)
+
+
+# get time variables
+# first have to change time zones
 old_times = wrf.extract_times(wrfin, timeidx=wrf.ALL_TIMES)
 _ = pd.DataFrame(old_times)  # some bs to fix time zone
 _[0] = _[0].dt.tz_localize("UTC")
 _[0] = _[0].dt.tz_convert("America/Phoenix")
 times = _[0].to_numpy()
 hours = [x.hour for x in times]
+# then we need to round to the nearest full day
 first_index = hours.index(0)
-# im willing to do a one liner because len(hours) is small
 last_index = len(hours) - hours[::-1].index(0) - 1
 times = times[first_index:last_index]
-
 old_date = pd.Timestamp(year=1950, month=1, day=1, tz="America/Phoenix")
 # days since 1950 jan 1 00:00:00
 days_since = [(x-old_date).days for x in times]
@@ -54,14 +52,15 @@ first_doy = pd.Timestamp(year=2019, month=12, day=31, tz='America/Phoenix')
 yearday = [(x-first_doy).days for x in times]
 yearday_var = list(set(yearday[first_index:last_index]))
 
+
 lat = ds['XLAT'][0,:,:]
 lon = ds['XLONG'][0,:,:]
+
 
 # the magic x and y numbers are from the actual eastings/northings values stuff
 #   from the lambert conformal projection that the wrf output uses.
 #   if the wrf projection changes, they should also change
 from pyproj import Proj
-
 # this string is from the wrf ncdump output
 proj_string = """
 +proj=lcc
@@ -81,17 +80,36 @@ easting_start, northing_start = proj(ds['XLONG'][0,0,0], ds['XLAT'][0,0,0])
 x_var = [easting_start + 1000*x for x in range(len(ds.west_east))]
 y_var = [northing_start + 1000*x for x in range(len(ds.south_north))]
 
+
+# make a bunch of raw variables and then transform them to fit the 1day format
 prcp_raw = ds['RAINNC'][first_index:last_index,:,:]
-# i want to sum up all precip for a day and store it back in there
+temp_raw = ds['T2'][first_index:last_index,:,:]
+srad_raw = ds['SWDOWN'][first_index:last_index,:,:]
+swe_raw = ds['SWE'][first_index:last_index,:,:]
+
+# transform data (ie prcp should be summed)
 prcp_var = np.zeros((len(yearday_var),prcp_raw.shape[1],prcp_raw.shape[2]))
-#temp_min_var = np.zeros((len(yearday_var),prcp_raw.shape[1],prcp_raw.shape[2]))
+tmin_var = np.zeros((len(yearday_var),temp_raw.shape[1],temp_raw.shape[2]))
+tmax_var = np.zeros((len(yearday_var),temp_raw.shape[1],temp_raw.shape[2]))
+srad_var = np.zeros((len(yearday_var),srad_raw.shape[1],srad_raw.shape[2]))
+swe_var = np.zeros((len(yearday_var),swe_raw.shape[1],swe_raw.shape[2]))
+vp_var = np.zeros((len(yearday_var),swe_raw.shape[1],swe_raw.shape[2]))
+
+vectorized_get_day_length = np.vectorize(get_day_length)
+doy_col = np.array(yearday)[:, np.newaxis, np.newaxis]  # todo idk about this
+dayl_var = vectorized_get_day_length(doy_col, lat)
+
 for t in range(len(yearday_var)):
     prcp_var[t,:,:] = np.sum(prcp_raw[t*24:t*24 + 24,:,:], axis=0)
-#    temp_min_var[t,:,:] = np.min()
+    tmin_var[t,:,:] = np.min(temp_raw[t*24:t*24 + 24,:,:], axis=0)
+    tmax_var[t,:,:] = np.max(temp_raw[t*24:t*24 + 24,:,:], axis=0)
+    srad_var[t,:,:] = np.average(srad_raw[t*24:t*24 + 24][srad_raw[t*24:t*24 + 24] > 0.0])
+    swe_var[t,:,:] = np.mean(swe_raw[t*24:t*24 + 24,:,:], axis=0)
 
-# todo unsure if needed
-#make fill values for empty stuff
-#prcp_var[prcp_var == 0] = -9999.0
+for t in range(first_index, last_index):
+    dewpoint = wrf.getvar(wrfin, 'td2', timeidx=t, units='K')
+    vp_var[t] = 6.11 * np.exp(5321 * (1/273 - 1/dewpoint))
+
 
 # make attributes for everything
 x_attr = dict(units='m', long_name='x coordinate of projection', standard_name = "projection_x_coordinate")
@@ -101,8 +119,19 @@ lon_attr = dict(units = "degrees_east", long_name = "longitude coordinate", stan
 time_attr = dict(standard_name = "time", calendar = "standard", units = "days since 1950-01-01 00:00:00", 
     bounds = "time_bnds", long_name = "24-hour day based on local time")
 yearday_attr = dict(long_name = "day of year (DOY) starting with day 1 on January 1st")
+# all of the actual variables
 prcp_attr = dict(_FillValue = -9999.0, long_name = "daily total precipitation", units = "mm/day",
     missing_value = -9999.0, coordinates = "lat lon", grid_mapping = "lambert_conformal_conic", cell_methods = "area: mean time: sum")
+dayl_attr = dict(_FillValue = -9999.0, long_name = "daylength", units = "s",
+    missing_value = -9999.0, coordinates = "lat lon", grid_mapping = "lambert_conformal_conic", cell_methods = "area: mean")
+swe_attr = dict(_FillValue = -9999.0, long_name = "snow water equivalent", units = "kg/m2",
+    missing_value = -9999.0, coordinates = "lat lon", grid_mapping = "lambert_conformal_conic", cell_methods = "area: mean time: mean")
+swrad_attr = dict(_FillValue = -9999.0, long_name = "daylight average incident shortwave radiation", units = "W/m2",
+    missing_value = -9999.0, coordinates = "lat lon", grid_mapping = "lambert_conformal_conic", cell_methods = "area: mean time: mean")
+# tmin
+# tmax
+# vp
+
 
 
 
@@ -125,6 +154,7 @@ new_ds = xr.Dataset(
         Conventions = "CF-1.6",
     )
 )
-
 new_ds.to_netcdf('/scratch/rswart/test.nc')
+new_ds.drop_vars('prcp')
+new_ds['dayl']
 
